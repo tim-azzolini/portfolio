fcc4d4d27932f9d78e39df7e4c0beb7083b77d3b
Tim Azzolini <82232601+tim-azzolini@users.noreply.github.com>
2024-03-27 19:36:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function openModal(imgElement) {
 function closeModal() {
     document.getElementById("modal").style.display = "none";
 }
+
+function filterImages(category) {
+    const images = document.querySelectorAll(".image-container img");
+
+    images.forEach(image => {
+        if (category === "featured" && !image.classList.contains("featured")) {
+            image.style.display = "none";
+        } else if (category === "digital" && !image.classList.contains("digital")) {
+            image.style.display = "none";
+        } else if (category === "film" && !image.classList.contains("film")) {
+            image.style.display = "none";
+        } else {
+            image.style.display = "block";
+        }
+    });
+}
